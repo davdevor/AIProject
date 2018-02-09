a88bae75da7145a73a8dac2e5bb4dfb9371d2ad7
David <drdcustomlows@gmail.com>
2018-02-09 17:28:14 -0500

    changed strings to lower

--- src/Oracle/Oracle.py
+++ src/Oracle/Oracle.py
@@ -2,18 +2,23 @@ import json
 
 def readPoems():
     word = input("Enter a topic: ")
+    word = str.lower(word)
     file = open('poetry.json',encoding = 'UTF-8')
     data = json.load(file)
     for x in data:
-        if(x['classification']==word):
-            print(x['text'])
+        if(str.lower(x['classification'])==word):
+            poem = x['text']
+            for s in poem:
+                print(s)
         for y in x['keywords']:
-            if(y==word):
-                print(x["text"])
+            if(word==str.lower(y)):
+                poem = x['text']
+                for s in poem:
+                    print(s)
                 return
-
+    print("No poem generated")
 def main():
     readPoems()
-    input()
+
 
 main()
