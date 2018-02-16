b2353e3203313cfa2ae36e6ca725db6006f2d778
David <drdcustomlows@gmail.com>
2018-02-16 16:24:11 -0500

    got more data

--- src/Baseline/wordsgetter.py
+++ src/Baseline/wordsgetter.py
@@ -10,8 +10,17 @@ baseurl = 'https://od-api.oxforddictionaries.com:443/api/v1/'
 url = baseurl+'/wordlist/'+language+'/lexicalCategory=noun'
 lex = 'nouns'
 r = requests.get("https://od-api.oxforddictionaries.com:443/api/v1/wordlist/en/lexicalCategory%3Dnoun", headers = {'app_id': app_id, 'app_key': app_key})
-print(r.status_code)
 jsonfile = open('nouns.json','w')
+json.dump(r.json(),jsonfile)
+jsonfile.close()
+
+r = requests.get("https://od-api.oxforddictionaries.com:443/api/v1/wordlist/en/lexicalCategory%3Dverb", headers = {'app_id': app_id, 'app_key': app_key})
+jsonfile = open('verbs.json','w')
+json.dump(r.json(),jsonfile)
+jsonfile.close()
+
+r = requests.get("https://od-api.oxforddictionaries.com:443/api/v1/wordlist/en/lexicalCategory%3Dadjective", headers = {'app_id': app_id, 'app_key': app_key})
+jsonfile = open('adjectives.json','w')
 json.dump(r.json(),jsonfile)
 jsonfile.close()
 input("enter")
